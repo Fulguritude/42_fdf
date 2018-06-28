2d438e048769e2bcd4782647ada87bfa5a548b32
fulguritude <duquesne.tristan@gmail.com>
2018-06-29 01:28:06 +0200

    better header handling for keycodes and portability

--- validation_format/fdf.h
+++ validation_format/fdf.h
@@ -29,6 +29,7 @@
 # include "libft/hdr/libft_str.h"
 # include "libft/hdr/get_next_line.h"
 # include "libft/hdr/ft_printf.h"
+# include "mlx_event_lin.h"
 
 # define MAX_LINE_SIZE	4096
 # define MAX_LINE_NB	4096
@@ -39,50 +40,6 @@
 # define REN_HEIGHT				800
 # define HALF_DRENWIN_WIDTH		100
 # define HALF_DRENWIN_HEIGHT	100
-
-/*
-** for keypad int codes /usr/include/X11/keysymdef.h
-*/
-
-# define L_CLICK		0x1
-# define R_CLICK		0x2
-# define SCROLL_UP		0x4
-# define SCROLL_DOWN	0x5
-
-# define KEY_ESC		0x35
-# define KEY_SPACE		0x31
-# define KEY_LCTRL		0x100
-
-# define KEY_LEFT		0x7B
-# define KEY_RIGHT		0x7C
-# define KEY_DOWN		0x7D
-# define KEY_UP			0x7E
-# define KEY_PAGE_UP	0x74
-# define KEY_PAGE_DN	0x79
-
-# define KEY_HOME		0x73
-# define KEY_END		0x77
-# define KEY_HELP		0x72
-# define KEY_DELETE		0x75
-
-# define KEY_NUMPAD_0			0x52
-# define KEY_NUMPAD_1			0x53
-# define KEY_NUMPAD_2			0x54
-# define KEY_NUMPAD_3			0x55
-# define KEY_NUMPAD_4			0x56
-# define KEY_NUMPAD_5			0x57
-# define KEY_NUMPAD_6			0x58
-# define KEY_NUMPAD_7			0x59
-# define KEY_NUMPAD_8			0x5B
-# define KEY_NUMPAD_9			0x5C
-# define KEY_NUMPAD_PERIOD		0x41
-# define KEY_NUMPAD_ENTER		0x4C
-# define KEY_NUMPAD_ADD			0x45
-# define KEY_NUMPAD_SUB			0x4E
-# define KEY_NUMPAD_MUL			0x43
-# define KEY_NUMPAD_DIV			0x4B
-# define KEY_NUMPAD_CLEAR		0x47
-# define KEY_NUMPAD_EQUALS		0x51
 
 # define PROJ_CST1		0.5
 # define PROJ_CST1_HLF	0.25
