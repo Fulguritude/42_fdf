edf504bfb22ce9e678ca84088158e1d6795984ff
Tristan DUQUESNE <tduquesn@e2r2p22.42.fr>
2018-06-28 17:14:21 +0200

    norme compliant fdf done, final debug

--- fdf_valid_format_with_macros.h
+++ fdf_valid_format_with_macros.h
@@ -6,7 +6,7 @@
 /*   By: fulguritude <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 15:35:26 by fulguritu         #+#    #+#             */
-/*   Updated: 2018/06/28 17:11:46 by tduquesn         ###   ########.fr       */
+/*   Updated: 2018/04/18 15:35:29 by fulguritu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,13 @@
 ** for keypad int codes /usr/include/X11/keysymdef.h
 */
 
+# define LINUX		0
+# define MACOS		1
+
+# define PLATFORM	LINUX
+
+
+#if PLATFORM
 # define L_CLICK		0x1
 # define R_CLICK		0x2
 # define SCROLL_UP		0x4
@@ -84,6 +91,63 @@
 # define KEY_NUMPAD_CLEAR		0x47
 # define KEY_NUMPAD_EQUALS		0x51
 
+
+#else
+/*
+**# define XK_KP_Space					0x0020
+**# define XK_KP_Tab						0xff89 //?
+**# define XK_KP_Enter					0xff8d
+**# define XK_KP_Left						0xff51
+**# define XK_KP_Up						0xff52
+**# define XK_KP_Right					0xff53
+**# define XK_KP_Down						0xff54
+**# define XK_KP_PageUp					0xff55
+**# define XK_KP_PageDown					0xff56
+**# define XK_KP_Esc						0xff1b
+**# define XK_KP_LCtrl					0xffe3
+*/
+# define L_CLICK		0x1
+# define M_CLICK		0x2
+# define R_CLICK		0x3
+# define SCROLL_UP		0x4
+# define SCROLL_DOWN	0x5
+
+# define KEY_ESC		0xFF1B
+# define KEY_SPACE		0x0020
+# define KEY_LCTRL		0xFFE3
+
+# define KEY_LEFT		0xFF51
+# define KEY_UP			0xFF52
+# define KEY_RIGHT		0xFF53
+# define KEY_DOWN		0xFF54
+# define KEY_PAGE_UP	0xFF55
+# define KEY_PAGE_DN	0xFF56
+
+# define KEY_HOME		0xFF50
+# define KEY_END		0xFF57
+# define KEY_INSERT		0xFF63
+# define KEY_DELETE		0xFFFF
+
+# define KEY_NUMPAD_0		0xFF9E
+# define KEY_NUMPAD_1		0xFF9C
+# define KEY_NUMPAD_2		0xFF99
+# define KEY_NUMPAD_3		0xFF9B
+# define KEY_NUMPAD_4		0xFF96
+# define KEY_NUMPAD_5		0xFF9D
+# define KEY_NUMPAD_6		0xFF98
+# define KEY_NUMPAD_7		0xFF95
+# define KEY_NUMPAD_8		0xFF97
+# define KEY_NUMPAD_9		0xFF9A
+# define KEY_NUMPAD_PERIOD	0xFF9F
+# define KEY_NUMPAD_ENTER	0xFF8D
+# define KEY_NUMPAD_ADD		0xFFAB
+# define KEY_NUMPAD_SUB		0xFFAD
+# define KEY_NUMPAD_MUL		0xFFAA
+# define KEY_NUMPAD_DIV		0xFFAF
+
+#endif
+
+
 # define PROJ_CST1		0.5
 # define PROJ_CST1_HLF	0.25
 # define PROJ_CST2		0.6
@@ -97,9 +161,6 @@
 
 # define INIT_H_SCALE	1.
 
-/*
-** make a union ?
-*/
 typedef struct	s_color
 {
 	t_u8			alpha;
@@ -108,9 +169,6 @@ typedef struct	s_color
 	t_u8			blue;
 }				t_color;
 
-/*
-** A pixel, with its coordinates and color
-*/
 typedef struct	s_gridpoint
 {
 	int				x;
@@ -119,9 +177,7 @@ typedef struct	s_gridpoint
 }				t_gridpoint;
 
 /*
-** Each vertex is a point in our worldspace
-** Model -> World -> View (cam space) -> Projection; where each -> is a matrix,
-**		the first matrix being the identity.
+** Model -> World -> View (cam space) -> Projection
 */
 typedef struct	s_vertex
 {
@@ -130,9 +186,6 @@ typedef struct	s_vertex
 	t_gridpoint		proj_pos;
 }				t_vertex;
 
-/*
-** A pair of two vertices to draw each "wire"
-*/
 typedef struct	s_edge
 {
 	t_vertex		*vtx_from;
@@ -140,14 +193,7 @@ typedef struct	s_edge
 }				t_edge;
 
 /*
-** vtx_lst_len	: length of the vertex list
-** edge_lst_len : length of the edge list
-** vtx_lst		: list of all points to plot
-** edge_lst		: list of all pairs of points that create a line to plot
-** w_trans		: x offset so that the map is centered in (0,0,0) of our wspace
-** h_trans		: y offset so that the map is centered in (0,0,0) of our wspace
-** h_scale		: parameter that scales the z:x == z:y ratio in our wspace,
-**					used to modify height of points before representation.
+** _trans is offset so that the map is centered in (0,0,0)
 */
 typedef struct	s_fdf
 {
@@ -161,13 +207,13 @@ typedef struct	s_fdf
 }				t_fdf;
 
 /*
-** world_pos		: cartesian coordinate of camera in world
-** reltv_pos		: cartesian coordinate of camera with anchor as center
-** polar_pos		: zoom/radius, longitude, latitude relative to anchor
-** anchor			: origin of polar_pos and reltv_pos
-** axis_x			: right vector of cam
-** axis_y			: up vector of cam
-** axis_z			: "forward" vector of cam,  input eye vector
+** world_pos; //cartesian coordinate of camera in world
+** reltv_pos; //cartesian coordinate of camera with anchor as center
+** polar_pos; //zoom/radius, longitude, latitude relative to anchor
+** anchor; //origin of polar_pos and reltv_pos
+** axis_x; //right
+** axis_y; //up
+** axis_z; //forward input eye
 */
 typedef struct	s_camera
 {
